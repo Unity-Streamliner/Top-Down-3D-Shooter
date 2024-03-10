a2c3cf664a6dc2467d3fbf1589decf746a0b5e08
tiger <thisisstreamliner@gmail.com>
2024-03-10 11:36:01 +0200

    Rig Aim Constraints

--- Assets/Scripts/Player/PlayerShooting.cs
+++ Assets/Scripts/Player/PlayerShooting.cs
@@ -44,7 +44,7 @@ public class PlayerShooting : MonoBehaviour
 
             transform.forward = _lookingDirection;
 
-            _aim.position = new Vector3(hitInfo.point.x, transform.position.y, hitInfo.point.z);
+            _aim.position = new Vector3(hitInfo.point.x, transform.position.y + 1, hitInfo.point.z);
         }
     }
 
