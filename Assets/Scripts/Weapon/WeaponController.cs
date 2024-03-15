3d999994c0fafaadddde6e9c81aec7accf4a4621
tiger <thisisstreamliner@gmail.com>
2024-03-15 16:34:15 +0200

    feat: implement left hand IK

--- Assets/Scripts/Weapon/WeaponController.cs
+++ Assets/Scripts/Weapon/WeaponController.cs
@@ -5,6 +5,9 @@ public class WeaponController : MonoBehaviour
     [SerializeField] private Transform[] _weapons;
     private Transform _currentWeapon;
 
+    [Header("Left Hand IK")]
+    [SerializeField] private Transform _leftHand;
+
     private void Start()
     {
         _currentWeapon = _weapons[0];
@@ -17,13 +20,21 @@ public class WeaponController : MonoBehaviour
         {
             if (i == weaponIndex)
             {
-                _weapons[i].gameObject.SetActive(true);
+                _currentWeapon = _weapons[i];
+                _currentWeapon.gameObject.SetActive(true);
+                AttachWeaponToHand();
             }
             else
             {
                 _weapons[i].gameObject.SetActive(false);
             }
         }
+    }
+
+    private void AttachWeaponToHand()
+    {
+        Transform targetTransform = _currentWeapon.GetComponentInChildren<LeftHandTargetTransform>().transform;
+        _leftHand.SetLocalPositionAndRotation(targetTransform.localPosition, targetTransform.localRotation);
     }
 }
 
